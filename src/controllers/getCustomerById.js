a16cf5eb015cb9e94d429c78caac007237791c7a
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-31 04:35:17 -0300

    Fix: Fixing customers birthday date format

--- src/controllers/getCustomerById.js
+++ src/controllers/getCustomerById.js
@@ -5,10 +5,10 @@ export default async function(req, res) {
     const { id } = req.params;
 
     try {
-        const search = await db.query("SELECT * FROM customers WHERE id = $1", [id]);
+        const search = await db.query("SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') as birthday FROM customers WHERE id = $1", [id]);
         if(search.rowCount === 0) return res.sendStatus(404); 
 
-        return res.send(search.rows);
+        return res.send(search.rows[0]);
     } catch (error) {
         res.sendStatus(500); 
     }
--- src/controllers/getCustomers.js
+++ src/controllers/getCustomers.js
@@ -3,7 +3,7 @@ import { db } from "../database.connection.js";
 export default async function(req, res) {
 
     try {
-        const search = await db.query("SELECT * FROM customers");
+        const search = await db.query("SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') as birthday FROM customers");
         return res.send(search.rows);
     } catch (error) {
         res.sendStatus(500); 
